6810c04c75a0def3695575393a99449f26732ea3
Al Taaj <altaaj23@gmail.com>
2025-03-24 22:19:18 +0700

    feat: implement balance card

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,6 +1,9 @@
 import { Link } from "expo-router";
-import { ScrollView, Text, StyleSheet, View } from "react-native";
+import { ScrollView, Text, StyleSheet, View, Image } from "react-native";
 import Greeting from "../../components/greetings.jsx";
+import eye from "../../assets/view.png";
+import plus from "../../assets/plus.png";
+import transfer from "../../assets/transfer.png";
 
 export default function HomeScreen() {
   return (
@@ -14,8 +17,22 @@ export default function HomeScreen() {
 
       {/* balance card */}
       <View style={styles.balanceCard}>
-        <Text style={styles.balance}>Balance</Text>
-        <Text style={styles.amount}>$ 1,000.00</Text>
+        <View style={styles.balanceRow}>
+          <Text style={styles.balance}>Balance</Text>
+          <View style={styles.balanceAmountRow}>
+            <Text style={styles.amount}>Rp 10.000.000</Text>
+            <Image style={styles.eye} source={eye} />
+          </View>
+        </View>
+
+        <View style={styles.actionRow}>
+          <View style={styles.actionButton}>
+            <Image source={plus} style={styles.actionIcon} />
+          </View>
+          <View style={styles.actionButton}>
+            <Image source={transfer} style={styles.actionIcon} />
+          </View>
+        </View>
       </View>
     </ScrollView>
   );
@@ -27,6 +44,7 @@ const styles = StyleSheet.create({
     padding: 20,
     backgroundColor: "#f7f9fb",
   },
+  // account styles
   account: {
     flexDirection: "row",
     justifyContent: "space-between",
@@ -41,10 +59,49 @@ const styles = StyleSheet.create({
     color: "#fff",
   },
 
+  // balance card styles
   balanceCard: {
     padding: 20,
     backgroundColor: "#fff",
     borderRadius: 10,
     marginVertical: 20,
+  },
+  balanceRow: {
+    marginBottom: 20,
+  },
+
+  balanceAmountRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 4,
+    gap: 8,
+  },
+
+  eye: {
+    width: 20,
+    height: 20,
+  },
+
+  amount: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+
+  actionRow: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    gap: 12,
+  },
+
+  actionButton: {
+    backgroundColor: "#0061FF",
+    borderRadius: 10,
+    padding: 10,
+  },
+
+  actionIcon: {
+    color: "#fff",
+    width: 20,
+    height: 20,
   },
 });
